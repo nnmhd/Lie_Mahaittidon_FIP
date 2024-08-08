cec9e99108c1a8e969cd7eb09c6b257b08ea0c7f
nnmhd <natchanone@gmail.com>
2024-08-07 20:46:31 -0400

    HTML and CSS update
    in this commit, HTML structure and CSS have been changed.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-let burger = document.querySelector("#burger-button");
-let burgerCon = document.querySelector("#burger-con");
+/* JS outline 
+1-Variable
+2-Function
+3-Events
+*/
 
-function hamburgerMenu() {
-  burger.classList.toggle("toggle");
-  burgerCon.classList.toggle("slide");
+// Variables
+let burgerBtn = document.querySelector("#burger-btn");
+let mainNav = document.querySelector("#main-nav");
+
+// Functions
+function showMenu() {
+  if (!mainNav.classList.contains("hide")) {
+    mainNav.classList.add("hide");
+    document.querySelector("#header").classList.remove("bg-green");
+    document.querySelector("#logo img").src = "images/sq-logo.svg";
+    burgerBtn.style.backgroundImage = 'url("../images/burger.svg")';
+  } else {
+    mainNav.classList.remove("hide");
+    document.querySelector("#header").classList.add("bg-green");
+    document.querySelector("#logo img").src = "images/sq-logo-white.svg";
+    burgerBtn.style.backgroundImage = 'url("../images/burger-reveal.svg")';
+  }
 }
 
-burger.addEventListener("click", hamburgerMenu, false);
+// Events
+burgerBtn.addEventListener("click", showMenu, false);
